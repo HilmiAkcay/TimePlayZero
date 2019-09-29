cccc8c5eba283f12484b8fa4a372a4170ff2399d
Hilmi Akçay <hilmiakcay@hotmail.com>
2019-09-29 22:37:18 +0300

    create item

--- src/app/items/create-item/create-item-dialog.component.ts
+++ src/app/items/create-item/create-item-dialog.component.ts
@@ -4,17 +4,22 @@ import { finalize } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { AppComponentBase } from '@shared/app-component-base';
 import {
-  ItemServiceProxy,
-  ItemDto,
-  ListResultDtoOfPermissionDto,
-  PermissionDto,
-  CreateItemDto
+    ItemServiceProxy,
+    ItemDto,
+    ListResultDtoOfPermissionDto,
+    PermissionDto,
+    CreateItemDto
 } from '@shared/service-proxies/service-proxies';
 
+export interface ItemType {
+    id: number;
+    value: string;
+}
+
 @Component({
-  templateUrl: 'create-Item-dialog.component.html',
-  styles: [
-    `
+    templateUrl: 'create-Item-dialog.component.html',
+    styles: [
+        `
       mat-form-field {
         width: 100%;
       }
@@ -22,84 +27,88 @@ import {
         padding-bottom: 5px;
       }
     `
-  ]
+    ]
 })
 export class CreateItemDialogComponent extends AppComponentBase
-  implements OnInit {
+    implements OnInit {
     saving = false;
     item: ItemDto = new ItemDto();
-  permissions: PermissionDto[] = [];
-  grantedPermissionNames: string[] = [];
-  checkedPermissionsMap: { [key: string]: boolean } = {};
-  defaultPermissionCheckedStatus = true;
+    permissions: PermissionDto[] = [];
+    grantedPermissionNames: string[] = [];
+    checkedPermissionsMap: { [key: string]: boolean } = {};
+    defaultPermissionCheckedStatus = true;
+    itemTypes: ItemType[] = [
+        { id: 0, value: 'Standard' },
+        { id: 1, value: 'Time Based' },
+    ];
 
-  constructor(
-    injector: Injector,
-    private _itemService: ItemServiceProxy,
-      private _dialogRef: MatDialogRef<CreateItemDialogComponent>
-  ) {
-    super(injector);
-  }
+    constructor(
+        injector: Injector,
+        private _itemService: ItemServiceProxy,
+        private _dialogRef: MatDialogRef<CreateItemDialogComponent>
+    ) {
+        super(injector);
+    }
 
-  ngOnInit(): void {
-    //this._itemService
-    //  .getAllPermissions()
-    //  .subscribe((result: ListResultDtoOfPermissionDto) => {
-    //    this.permissions = result.items;
-    //    this.setInitialPermissionsStatus();
-    //  });
-  }
+    ngOnInit(): void {
+        //this._itemService
+        //  .getAllPermissions()
+        //  .subscribe((result: ListResultDtoOfPermissionDto) => {
+        //    this.permissions = result.items;
+        //    this.setInitialPermissionsStatus();
+        //  });
+    }
 
-  setInitialPermissionsStatus(): void {
-    _.map(this.permissions, item => {
-      this.checkedPermissionsMap[item.name] = this.isPermissionChecked(
-        item.name
-      );
-    });
-  }
+    setInitialPermissionsStatus(): void {
+        _.map(this.permissions, item => {
+            this.checkedPermissionsMap[item.name] = this.isPermissionChecked(
+                item.name
+            );
+        });
+    }
 
-  isPermissionChecked(permissionName: string): boolean {
-    // just return default permission checked status
-    // it's better to use a setting
-    return this.defaultPermissionCheckedStatus;
-  }
+    isPermissionChecked(permissionName: string): boolean {
+        // just return default permission checked status
+        // it's better to use a setting
+        return this.defaultPermissionCheckedStatus;
+    }
 
-  onPermissionChange(permission: PermissionDto, $event: MatCheckboxChange) {
-    this.checkedPermissionsMap[permission.name] = $event.checked;
-  }
+    onPermissionChange(permission: PermissionDto, $event: MatCheckboxChange) {
+        this.checkedPermissionsMap[permission.name] = $event.checked;
+    }
 
-  getCheckedPermissions(): string[] {
-    const permissions: string[] = [];
-    _.forEach(this.checkedPermissionsMap, function(value, key) {
-      if (value) {
-        permissions.push(key);
-      }
-    });
-    return permissions;
-  }
+    getCheckedPermissions(): string[] {
+        const permissions: string[] = [];
+        _.forEach(this.checkedPermissionsMap, function (value, key) {
+            if (value) {
+                permissions.push(key);
+            }
+        });
+        return permissions;
+    }
 
-  save(): void {
-    this.saving = true;
+    save(): void {
+        this.saving = true;
 
-    //this.role.grantedPermissions = this.getCheckedPermissions();
+        //this.role.grantedPermissions = this.getCheckedPermissions();
 
-      const item_ = new CreateItemDto();
-      item_.init(this.item);
+        const item_ = new CreateItemDto();
+        item_.init(this.item);
 
-    this._itemService
-      .create(item_)
-      .pipe(
-        finalize(() => {
-          this.saving = false;
-        })
-      )
-      .subscribe(() => {
-        this.notify.info(this.l('SavedSuccessfully'));
-        this.close(true);
-      });
-  }
+        this._itemService
+            .create(item_)
+            .pipe(
+                finalize(() => {
+                    this.saving = false;
+                })
+            )
+            .subscribe(() => {
+                this.notify.info(this.l('SavedSuccessfully'));
+                this.close(true);
+            });
+    }
 
-  close(result: any): void {
-    this._dialogRef.close(result);
-  }
+    close(result: any): void {
+        this._dialogRef.close(result);
+    }
 }
